0e1a07a8be1fd3a089e1915246535453dde3f8cb
navxdev <navneetxdev@gmail.com>
2025-07-15 12:36:44 +0530

    updated pattern added

--- PS C-1/patterns.py
+++ PS C-1/patterns.py
@@ -38,5 +38,7 @@ for i in range(4):
         print(" # " , end="")
     print("")
 
-for i in range(5):
-    print("# " * i)
+for i in range(6):
+    for j in range(i):
+        print("# " , end ="")
+    print()
